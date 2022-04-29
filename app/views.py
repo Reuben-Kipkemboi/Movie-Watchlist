95d2328203a1d5d94a6840fb21dc69d082f27776
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-29 14:53:02 +0300

    ft:nav-bar - creates a navbar

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from.request import get_movies
+from.request import get_movies, get_movie
 
 #Views
 @app.route('/')
@@ -22,3 +22,14 @@ def movie(movie_id):
     '''
     # title = "my id"
     return render_template('movie.html', id = movie_id)
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
