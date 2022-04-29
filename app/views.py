f6a87255d7acff6afce2d315b8c12f5d2229a895
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-29 10:36:22 +0300

    ft:api-request - makes na API cal request

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from.request import get_movies
 
 #Views
 @app.route('/')
@@ -7,10 +8,11 @@ def index():
     """
     View root page function that returns the index page and its data
     """
-    title = "welcome to my movie watchlist"
-    message = "Hellow Friday am making revision"
-    # return render_template('index.html')
-    return render_template("index.html",title = title, message = message)
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
