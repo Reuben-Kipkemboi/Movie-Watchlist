7373048959374794f3833dd2cc90fcefadc15f7b
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-03 20:09:01 +0300

    ft:user model - creates a password hashing feature

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db #
+from werkzeug.security import generate_password_hash,check_password_hash # hashing password
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -51,6 +52,18 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id')) #will help us link the two tables/models
+    pass_secure = db.Column(db.String(255))# holding our passwords
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
